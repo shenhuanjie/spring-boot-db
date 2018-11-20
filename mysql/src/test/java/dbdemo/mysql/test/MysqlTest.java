635fb5b75f16723822e85a30ac5c83b27d4e9877
shenhuanjie <shenhuanjie@live.cn>
2018-11-20 17:29:41 +0800

    Update MysqlTest.java

--- mysql/src/test/java/dbdemo/mysql/test/MysqlTest.java
+++ mysql/src/test/java/dbdemo/mysql/test/MysqlTest.java
@@ -54,7 +54,7 @@ public class MysqlTest {
         User user = new User();
         user.setName("user");
         user.setCreatedate(new Date());
-        user.setDeparment(department);
+        user.setDepartment(department);
 
         List<Role> roles = roleRepository.findAll();
         Assert.notNull(roles);
@@ -71,7 +71,7 @@ public class MysqlTest {
         Assert.notNull(page);
         for(User user : page.getContent()) {
             logger.info("====user==== user name:{}, department name:{}, role name:{}",
-                    user.getName(), user.getDeparment().getName(), user.getRoles().get(0).getName());
+                    user.getName(), user.getDepartment().getName(), user.getRoles().get(0).getName());
         }
     }
 }
